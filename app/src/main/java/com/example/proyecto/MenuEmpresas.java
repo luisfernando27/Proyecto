3de984db036a731ff7b2e9dc25523dbbd820d769
luisfernando27 <LZ20020098@zacatecassur.tecnm.mx>
2024-05-25 14:42:09 -0600

    Funcionalidad de cerrar sesion

--- app/src/main/java/com/example/proyecto/MenuEmpresas.java
+++ app/src/main/java/com/example/proyecto/MenuEmpresas.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -61,10 +62,20 @@ public class MenuEmpresas extends AppCompatActivity implements NavigationView.On
         } else if (itemId == R.id.nav_info_us) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoclientFragment()).commit();
         } else if (itemId == R.id.nav_logout) {
-            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            logout();
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void logout() {
+        // Mostrar mensaje de cierre de sesión
+        Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+
+        // Redirigir a la pantalla de inicio de sesión
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish(); // Finalizar la actividad actual
     }
     @Override
     public void onBackPressed() {
