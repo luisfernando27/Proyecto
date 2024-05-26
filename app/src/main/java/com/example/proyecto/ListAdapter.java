8a4ff67ded3b90527f5a91e0e4152431d06ab0f8
luisfernando27 <LZ20020098@zacatecassur.tecnm.mx>
2024-05-26 10:26:42 -0600

    Modulo de ofertas terminado

--- app/src/main/java/com/example/proyecto/ListAdapter.java
+++ app/src/main/java/com/example/proyecto/ListAdapter.java
@@ -29,6 +29,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     private Context mContext; // Mantener una referencia al contexto
     private String userEmail, idC;
 
+
     // Constructor que acepta la lista de elementos, el contexto y el correo electrónico del usuario
     public ListAdapter(List<ListaElementosClientes> elements, Context context, String userEmail) {
         this.mData = elements;
@@ -95,7 +96,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
                     int cantidad = Integer.parseInt(cantidadStr);
                     registrarOfertaEnClientes(oferta, cantidad);
-
+                    cantidadIngresada.setText("");
                 }
             });
         }
@@ -113,168 +114,105 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     // Método para registrar la oferta en la base de datos de clientes
     private void registrarOfertaEnClientes(ListaElementosClientes oferta, int cantidadSolicitada) {
-
-        // Continuar con la consulta si el campo de correo no está vacío
         AdminSqLite admin = new AdminSqLite(mContext, "localMarket", null, 1);
         SQLiteDatabase bd = admin.getWritableDatabase();
-
-        // Realizar la consulta en la base de datos
 
         Cursor cursorID = bd.rawQuery("SELECT idC FROM clientes WHERE correo='" + userEmail + "'", null);
         if (cursorID.moveToFirst()) {
             idC = cursorID.getString(0);
         }
 
+        Cursor cursor_fecha = bd.rawQuery("SELECT fecha_inicio, fecha_fin, total_oferta, maximo_clientes FROM ofertasEmpresas WHERE idF='" + oferta.getID() + "'", null);
+        Cursor maximo = bd.rawQuery("SELECT cantidad_maxima FROM ofertaCliente WHERE idC='" + idC + "' and idF='" + oferta.getID() + "'", null);
 
-        Cursor cursor_fecha = bd.rawQuery("SELECT fecha_fin, total_oferta, maximo_clientes FROM ofertasEmpresas WHERE idF='" + oferta.getID() + "'", null);
-        Cursor maximo = bd.rawQuery("SELECT cantidad_maxima FROM ofertaCliente WHERE idC='" + idC + "' and  idF='" + oferta.getID() + "'", null);
-        if (maximo.moveToFirst()) {
-            if (cursor_fecha.moveToFirst()) {
-                String fecha_fin = cursor_fecha.getString(0);
-                int total_oferta = cursor_fecha.getInt(1);
-                int maximo_clientes = cursor_fecha.getInt(2);
+        if (!cursor_fecha.moveToFirst()) {
+            Toast.makeText(mContext, "Error al obtener la oferta", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-                Calendar fechaActual = Calendar.getInstance();
-                int mesActual = fechaActual.get(Calendar.MONTH) + 1;
-                String formatoHoy;
-                if (fechaActual.get(Calendar.DAY_OF_MONTH) < 10) {
-                    formatoHoy = fechaActual.get(Calendar.DAY_OF_MONTH) + "/" + mesActual + "/0" + fechaActual.get(Calendar.YEAR);
-                    Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-                } else {
-                    formatoHoy = fechaActual.get(Calendar.DAY_OF_MONTH) + "/" + mesActual + "/" + fechaActual.get(Calendar.YEAR);
-                    Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-                }
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String fecha_inicio = cursor_fecha.getString(0);
+        String fecha_fin = cursor_fecha.getString(1);
+        int total_oferta = cursor_fecha.getInt(2);
+        int maximo_clientes = cursor_fecha.getInt(3);
 
-                try {
-                    Date fechaFinDate = dateFormat.parse(fecha_fin);
-                    Date fechaHoyDate = dateFormat.parse(formatoHoy);
+        Calendar fechaActual = Calendar.getInstance();
+        int mesActual = fechaActual.get(Calendar.MONTH) + 1;
+        String formatoHoy = String.format(Locale.getDefault(), "%02d/%02d/%04d",
+                fechaActual.get(Calendar.DAY_OF_MONTH), mesActual, fechaActual.get(Calendar.YEAR));
 
-                    if (fechaHoyDate.after(fechaFinDate)) {
-                        Toast.makeText(mContext, "La oferta ha expirado ", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else {
-                    }
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    Log.e("ERROR", "Error parsing dates: " + e.getMessage());
-                }
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        try {
+            Date fechaInicioDate = dateFormat.parse(fecha_inicio);
+            Date fechaFinDate = dateFormat.parse(fecha_fin);
+            Date fechaHoyDate = dateFormat.parse(formatoHoy);
 
-                Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-
-                int cantidad_maxima = maximo.getInt(0);
-
-                if (cantidad_maxima < maximo_clientes) {
-                    int total = cantidad_maxima + cantidadSolicitada;
-                    if (total <= maximo_clientes) {
-                        // Actualizar la base de datos
-                        ContentValues valores = new ContentValues();
-                        valores.put("cantidad_maxima", total);
-
-                        // Asumiendo que bd es una instancia de SQLiteDatabase
-                        int rowsUpdated = bd.update("ofertaCliente", valores, "idC=? and idF=?", new String[]{idC, oferta.getID()});
-
-                        if (rowsUpdated > 0) {
-                            Toast.makeText(mContext, "Compra realizada con éxito", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(mContext, "Error al actualizar la oferta", Toast.LENGTH_SHORT).show();
-                        }
-
-                        int ttotal=total_oferta-cantidadSolicitada;
-                        String nuevoTTotalOfertaString = String.valueOf(ttotal);
-
-                        ContentValues valores1 = new ContentValues();
-                        valores1.put("total_oferta", nuevoTTotalOfertaString);
-
-                        // Asumiendo que bd es una instancia de SQLiteDatabase
-                        int rowsUpdated1 = bd.update("ofertasEmpresas", valores1, "idF=?", new String[]{oferta.getID()});
-
-                        if (rowsUpdated1 > 0) {
-                            Toast.makeText(mContext, "Compra realizada con éxito", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(mContext, "Error al actualizar la oferta", Toast.LENGTH_SHORT).show();
-                        }
-
-
-                    } else {
-                        int limite = maximo_clientes - cantidad_maxima;
-                        Toast.makeText(mContext, "Las ofertas que puedes comprar son: " + limite, Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(mContext, "Tienes el maximo de ofertas", Toast.LENGTH_SHORT).show();
-                }
+            if (fechaHoyDate.after(fechaFinDate)) {
+                Toast.makeText(mContext, "La oferta ha expirado ", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (fechaHoyDate.before(fechaInicioDate)) {
+                Toast.makeText(mContext, "La oferta no ha comenzado ", Toast.LENGTH_SHORT).show();
+                return;
             }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Log.e("ERROR", "Error parsing dates: " + e.getMessage());
+            return;
+        }
+
+        if (cantidadSolicitada > total_oferta) {
+            Toast.makeText(mContext, "Cantidad de oferta insuficiente", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (cantidadSolicitada > maximo_clientes) {
+            Toast.makeText(mContext, "Solo puedes comprar máximo " + maximo_clientes + " ofertas", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (cantidadSolicitada == 0) {
+            Toast.makeText(mContext, "Ingresa un número correcto", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
 
-        }else{
-            if (cursor_fecha.moveToFirst()) {
-                String fecha_fin = cursor_fecha.getString(0);
-                int total_oferta = cursor_fecha.getInt(1);
-                int maximo_clientes = cursor_fecha.getInt(2);
-
-                Calendar fechaActual = Calendar.getInstance();
-                int mesActual = fechaActual.get(Calendar.MONTH) + 1;
-                String formatoHoy;
-                if (fechaActual.get(Calendar.DAY_OF_MONTH) < 10) {
-                    formatoHoy = fechaActual.get(Calendar.DAY_OF_MONTH) + "/" + mesActual + "/0" + fechaActual.get(Calendar.YEAR);
-                    Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-                } else {
-                    formatoHoy = fechaActual.get(Calendar.DAY_OF_MONTH) + "/" + mesActual + "/" + fechaActual.get(Calendar.YEAR);
-                    Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-                }
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-                try {
-                    Date fechaFinDate = dateFormat.parse(fecha_fin);
-                    Date fechaHoyDate = dateFormat.parse(formatoHoy);
-
-                    if (fechaHoyDate.after(fechaFinDate)) {
-                        Toast.makeText(mContext, "La oferta ha expirado ", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else {
-                    }
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    Log.e("ERROR", "Error parsing dates: " + e.getMessage());
-                }
-
-                Log.d("FORMATO", "fechaHoyMetodo: " + formatoHoy);
-
-                if (cantidadSolicitada > total_oferta) {
-                    Toast.makeText(mContext, "Cantidad de oferta insuficiente", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (cantidadSolicitada > maximo_clientes) {
-                    Toast.makeText(mContext, "Solo puedes comprar maximo " + maximo_clientes + " ofertas", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (cantidadSolicitada == 0) {
-                    Toast.makeText(mContext, "Ingresa un número correcto", Toast.LENGTH_SHORT).show();
-                    return;
-                } else {
-                    ContentValues registro = new ContentValues();
-                    registro.put("idC", idC);
-                    registro.put("idF", oferta.getID());
-                    registro.put("cantidad_maxima", cantidadSolicitada);
-
-                    bd.insert("ofertaCliente", null, registro);
-
-                    int nuevoTotalOferta = total_oferta - cantidadSolicitada;
-
-                    String nuevoTotalOfertaString = String.valueOf(nuevoTotalOferta);
-
-                    // Actualizar la base de datos
+        if (maximo.moveToFirst()) {
+            int cantidad_maxima = maximo.getInt(0);
+            if (cantidad_maxima < maximo_clientes) {
+                int total = cantidad_maxima + cantidadSolicitada;
+                if (total <= maximo_clientes) {
                     ContentValues valores = new ContentValues();
-                    valores.put("total_oferta", nuevoTotalOfertaString);
+                    valores.put("cantidad_maxima", total);
 
-                    // Asumiendo que bd es una instancia de SQLiteDatabase
-                    int rowsUpdated = bd.update("ofertasEmpresas", valores, "idF=?", new String[]{oferta.getID()});
-
+                    int rowsUpdated = bd.update("ofertaCliente", valores, "idC=? and idF=?", new String[]{idC, oferta.getID()});
                     if (rowsUpdated > 0) {
                         Toast.makeText(mContext, "Compra realizada con éxito", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(mContext, "Error al actualizar la oferta", Toast.LENGTH_SHORT).show();
                     }
+
+                    int nuevoTotalOferta = total_oferta - cantidadSolicitada;
+                    ContentValues valores1 = new ContentValues();
+                    valores1.put("total_oferta", nuevoTotalOferta);
+
+                    int rowsUpdated1 = bd.update("ofertasEmpresas", valores1, "idF=?", new String[]{oferta.getID()});
+                    if (rowsUpdated1 > 0) {
+                        Toast.makeText(mContext, "Compra realizada con éxito", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(mContext, "Error al actualizar la oferta", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    int limite = maximo_clientes - cantidad_maxima;
+                    Toast.makeText(mContext, "Las ofertas que puedes comprar son: " + limite, Toast.LENGTH_SHORT).show();
                 }
+            }else {
+                Toast.makeText(mContext, "Tienes el maximo de ofertas", Toast.LENGTH_SHORT).show();
             }
+
+        } else {
+            ContentValues registro = new ContentValues();
+            registro.put("idC", idC);
+            registro.put("idF", oferta.getID());
+            registro.put("cantidad_maxima", cantidadSolicitada);
+
+            bd.insert("ofertaCliente", null, registro);
+            Toast.makeText(mContext, "Compra realizada con éxito", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
